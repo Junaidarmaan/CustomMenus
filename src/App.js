117c34be9ddc9ee38af427904432aa793cc94ed5
junaid <junnubest@gmail.com>
2025-03-01 19:56:27 +0530

    added images to public folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import SideNavBar from './components/SideNavBar'
+import Menu from './components/Menu'
+import "./App.css"
 
 
 export default function App() {
   return (
-    <div>
+    <div className='container'>
       <SideNavBar/>
+      <Menu/>
     </div>
   )
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../CSS/Menu.css'
+
+export default function Menu() {
+    return (
+        <>
+            <div class="menu-container">
+                <div class="menu-item">
+                    <img src="" alt="Item 1" class="item-image" />
+                    <h3 class="item-name">Item 1</h3>
+                    <p class="item-price">Price: ₹150</p>
+                    <p class="item-description">Delicious and freshly prepared.</p>
+                </div>
+
+            </div>
+
+
+        </>
+    )
+}
